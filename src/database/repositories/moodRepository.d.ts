de0f9ea862536f186b423a00821d38e15e09f7e2
Bence Markus <markus.bence@icloud.com>
2023-05-15 01:27:03 +0200

    mood repository and definition added

---
+++ src/database/repositories/moodRepository.d.ts
@@ -0,0 +1,96 @@
+import type { Mood, Prisma } from '@prisma/client';
+
+export type TFindMoodByIdArgs = {
+	id: number;
+};
+
+export type TFindDefaultsWithUserArgs = {
+	userId: number;
+};
+
+export type TFindMoodsByUserIdArgs = {
+	userId: number;
+};
+
+export type TCreateMoodArgs = {
+	mood: Prisma.MoodCreateInput;
+};
+
+export type TUpdateMoodArgs = {
+	id: number;
+	mood: Prisma.MoodUpdateInput;
+};
+
+export type TDeleteMoodArgs = {
+	id: number;
+};
+
+export type TRestoreMoodArgs = {
+	id: number;
+};
+
+export type TMoodRepositoryConstructorArgs = {
+	delegate: MoodRepository;
+};
+
+/**
+ * Interface definition for MoodRepositories
+ */
+export interface TMoodRepository {
+	/**
+	 * Uses the dependency injected delegate to return a single mood by id or null if
+	 * the mood not found
+	 *
+	 * @throws InvalidArgumentError if the id is not a valid numeric id
+	 */
+	findById({ id }: TFindMoodByIdArgs): Promise<Mood | null>;
+	/**
+	 * Uses the dependency injected delegate to return multiple moods by userId or empty
+	 * array if no moods were found
+	 *
+	 * @throws InvalidArgumentError if the userId is not a valid numeric id
+	 */
+	findByUserId({ userId }: TFindMoodsByUserIdArgs): Promise<Mood[]>;
+	/**
+	 * Uses the dependency injected delegate to return the default and user created moods
+	 * or empty array if no moods were found
+	 *
+	 * @throws InvalidArgumentError if the userId is not a valid numeric id
+	 */
+	findDefaultsWithUser({ userId }: TFindDefaultsWithUserArgs): Promise<Mood[]>;
+	/**
+	 * Uses the dependency injected delegate to create a mood
+	 *
+	 * @throws InvalidPayloadError if the payload is invalid or missing required fields
+	 */
+	create({ mood }: TCreateMoodArgs): Promise<Mood>;
+	/**
+	 * Uses the dependency injected delegate to update a mood
+	 *
+	 * @throws InvalidArgumentError if the id is not a valid numeric id
+	 * @throws InvalidPayloadError if the payload is invalid or missing required fields
+	 */
+	update({ id, mood }: TUpdateMoodArgs): Promise<Mood>;
+	/**
+	 * Uses the dependency injected delegate to soft delete a mood
+	 * This is the preferred method of deleting a mood
+	 *
+	 * @throws InvalidArgumentError if the id is not a valid numeric id
+	 */
+	softDelete({ id }: TDeleteMoodArgs): Promise<void>;
+	/**
+	 * Uses the dependency injected delegate to restore a soft deleted mood
+	 *
+	 * @throws InvalidArgumentError if the id is not a valid numeric id
+	 */
+	restore({ id }: TRestoreMoodArgs): Promise<void>;
+	/**
+	 * Uses the dependency injected delegate to permanently delete a mood
+	 * This should only be used when absolutely necessary. The preferred method
+	 * of deleting a mood is to soft delete it
+	 * @see softDelete
+	 *
+	 * @throws InvalidArgumentError if the id is not a valid numeric id
+	 */
+	hardDelete({ id }: TDeleteMoodArgs): Promise<void>;
+}
---
+++ src/database/repositories/moodRepository.ts
@@ -0,0 +1,69 @@
+import type {
+	TDeleteMoodArgs,
+	TCreateMoodArgs,
+	TUpdateMoodArgs,
+	TFindMoodByIdArgs,
+	TFindMoodsByUserIdArgs,
+	TFindDefaultsWithUserArgs,
+} from './moodRepository.d';
+import Database from '../database';
+import type { Mood, Prisma } from '@prisma/client';
+import type { TMoodRepository, TMoodRepositoryConstructorArgs } from './moodRepository.d';
+
+let sharedInstance: MoodRepository | null = null;
+
+export default class MoodRepository implements TMoodRepository {
+	private readonly delegate: Prisma.MoodDelegate<false>;
+
+	static get sharedInstance(): MoodRepository {
+		if (sharedInstance === null) {
+			sharedInstance = new MoodRepository({
+				delegate: Database.sharedInstance.getDefaultClient().mood,
+			});
+		}
+		return sharedInstance;
+	}
+
+	constructor({ delegate }: TMoodRepositoryConstructorArgs) {
+		this.delegate = delegate;
+	}
+
+	public async findById({ id }: TFindMoodByIdArgs): Promise<Mood | null> {
+		const queryResult = await this.delegate.findUnique({ where: { id } });
+		return queryResult;
+	}
+
+	public async findByUserId({ userId }: TFindMoodsByUserIdArgs): Promise<Mood[]> {
+		const queryResult = await this.delegate.findMany({ where: { userId } });
+		return queryResult;
+	}
+
+	public async findDefaultsWithUser({ userId }: TFindDefaultsWithUserArgs): Promise<Mood[]> {
+		const queryResult = await this.delegate.findMany({
+			where: { OR: [{ userId }, { userId: null }] },
+		});
+		return queryResult;
+	}
+
+	public async create({ mood }: TCreateMoodArgs): Promise<Mood> {
+		const queryResult = await this.delegate.create({ data: mood });
+		return queryResult;
+	}
+
+	public async update({ id, mood }: TUpdateMoodArgs): Promise<Mood> {
+		const queryResult = await this.delegate.update({ where: { id }, data: mood });
+		return queryResult;
+	}
+
+	public async softDelete({ id }: TDeleteMoodArgs): Promise<void> {
+		await this.delegate.update({ where: { id }, data: { isActive: false } });
+	}
+
+	public async restore({ id }: TDeleteMoodArgs): Promise<void> {
+		await this.delegate.update({ where: { id }, data: { isActive: true } });
+	}
+
+	public async hardDelete({ id }: TDeleteMoodArgs): Promise<void> {
+		await this.delegate.delete({ where: { id } });
+	}
+}
