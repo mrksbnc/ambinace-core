ce1de80d11e377d513cb3642dd3410a286044506
Bence Markus <markus.bence@icloud.com>
2023-05-15 01:54:46 +0200

    refactor

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,12 @@ import { cleanEnv, num, port, str } from 'envalid';
 	cleanEnv(process.env, {
 		PORT: port(),
 		NODE_ENV: str(),
-		DATABASE_URL: str(),
+		BASE_URL: str(),
 		JWT_SECRET: str(),
+		DATABASE_URL: str(),
 		JWT_EXPIRES_IN: str(),
 		JWT_GRACE_PERIOD: str(),
 		BCRYPT_SALT_ROUNDS: num(),
-		BASE_URL: str(),
 	});
 
 	dotenv.config();
