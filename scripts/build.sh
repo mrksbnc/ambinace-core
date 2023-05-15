cd429162a30c08853feb45d5fb1f92b779efedc1
Bence Markus <markus.bence@icloud.com>
2023-05-16 01:13:05 +0200

    bild script update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
 if [ -d "dist" ]; then
-  echo "Build directory already exists..."
-	echo "Removing old build directory..."
+  echo "> Build directory already exists..."
+	echo "> Removing old build directory..."
 	rm -rf dist
 else
-	echo "Build directory does not exist..."
+	echo "> Build directory does not exist..."
 fi
 
-echo "Creating new build directory..."
+echo "> Creating new build directory..."
 mkdir dist
 
-echo "Building..."
+echo "> Compiling TypeScript files..."
 node scripts/build.js
